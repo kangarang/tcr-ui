0b2fbead5c377353344f6e13e62291e63c435513
Isaac Kang <isaac.kang@consensys.net>
2017-12-17 13:52:17 -0800

    https

--- server.js
+++ server.js
@@ -1,24 +1,21 @@
-const express = require("express");
-const path = require("path");
+const express = require('express')
+const path = require('path')
 
-const app = express();
+const app = express()
 
-// app.use(function(req, res, next) {
-//   if((!req.secure) && (req.get('X-Forwarded-Proto') !== 'https')) {
-//       res.redirect('https://' + req.get('Host') + req.url);
-//     }
-//   else
-//     next();
-// });
+app.use(function(req, res, next) {
+  if (!req.secure && req.get('X-Forwarded-Proto') !== 'https') {
+    res.redirect('https://' + req.get('Host') + req.url)
+  } else next()
+})
 
 /* static route */
-app.use(express.static(__dirname + "/build"));
+app.use(express.static(__dirname + '/build'))
 
 /* config for browser history in react */
-app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "index.html")));
+app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'index.html')))
 
 const server = app.listen(process.env.PORT || 3000, () => {
-  const port = server.address().port;
-  console.log("app listening on port: ", port);
-});
-
+  const port = server.address().port
+  console.log('app listening on port: ', port)
+})
