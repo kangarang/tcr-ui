aae6436a2459fd2034136e9d2686cf0e2aaa3f39
Isaac Kang <isaac.kang@consensys.net>
2018-01-23 01:07:50 -0500

    Input styles

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -8,11 +8,12 @@ import { colors } from '../Colors'
 
 const Input = styled.input`
   width: 100%;
-  border: none;
-  border-bottom: 1px solid ${colors.prism};
-  font-size: 1em;
-  padding: .2em;
+  box-sizing: border-box;
+  border: 1px solid ${colors.prism};
+  font-size: 1.3em;
+  padding: .5em;
   color: ${colors.offBlack};
-  
+  background-color: ${colors.offWhite};
+  font-family: monospace;
 `
 export default Input;
