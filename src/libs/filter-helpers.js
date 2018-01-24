bc24364ab245260c7d0726e582d6e7a9175debac
Isaac Kang <isaac.kang@consensys.net>
2018-01-24 11:26:31 -0500

    add promisify

---
+++ src/libs/filter-helpers.js
@@ -0,0 +1,6 @@
+
+// converts async fn to return a promise
+export const promisify = func => (...args) =>
+  new Promise((resolve, reject) =>
+    func(...args, (err, result) => (err ? reject(err) : resolve(result)))
+  )
