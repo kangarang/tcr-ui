4425a38a0fd6c6ec4ac6f17f81eb06a2b2267a8c
Isaac Kang <isaac.kang@consensys.net>
2018-01-31 16:07:22 -0500

    icon style

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -20,30 +20,11 @@ const icons = {
 }
 
 export default props => {
-  const styles = {
-    svg: {
-      display: 'inline-block',
-      verticalAlign: 'middle',
-    },
-    path: {
-      fill: props.color,
-    },
-    img: {
-      backgroundColor: `${colors.blue1}`,
-    },
+  const style = {
+    display: 'inline-block',
+    verticalAlign: 'middle',
+    backgroundColor: `${colors.blue1}`,
   }
 
-  return (
-    // <svg
-    //   style={styles.svg}
-    //   width={10}
-    //   height={10}
-    //   // width={`${props.size}px`}
-    //   // height={`${props.size}px`}
-    //   viewBox="0 0 1024 1024"
-    // >
-    //   <path style={styles.path} d={icons.globe} />
-    // </svg>
-    <img src={icons[props.iconName]} alt={props.iconName} style={styles.img} />
-  )
+  return <img src={icons[props.iconName]} alt={props.iconName} style={style} />
 }
