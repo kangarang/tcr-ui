d03c1f9e7ee4f4dca1e6c59d549df286b6eb0507
Isaac Kang <isaac.kang@consensys.net>
2018-02-04 02:37:51 -0500

    categorize constants

--- src/actions/constants.js
+++ src/actions/constants.js
@@ -1,15 +1,23 @@
-export const CONTRACT_ERROR = 'CONTRACT_ERROR--|--Home'
-export const LOGS_ERROR = 'LOGS_ERROR--|--Home'
-export const LOGIN_ERROR = 'LOGIN_ERROR--|--Home'
+// Ethereum
+export const SET_ADDRESS = 'SET_ADDRESS--|--ETH'
+export const GET_ETH_PROVIDER = 'GET_ETH_PROVIDER--|--ETH'
+export const SET_FROM_ADDRESS = 'SET_FROM_ADDRESS--|--ETH'
+export const REFRESH_ETH_STORE = 'REFRESH_ETH_STORE--|--ETH'
+
+// Errors
+export const CONTRACT_ERROR = 'CONTRACT_ERROR--|--Error'
+export const LOGS_ERROR = 'LOGS_ERROR--|--Error'
+export const LOGIN_ERROR = 'LOGIN_ERROR--|--Error'
+export const UDAPP_ERROR = 'UDAPP_ERROR--|--Error'
+
+// Home
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS--|--Home'
 export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS--|--Home'
 export const WALLET_ERROR = 'WALLET_ERROR--|--Home'
 export const GET_ETHEREUM = 'GET_ETHEREUM--|--Home'
 export const DELETE_LISTINGS = 'DELETE_LISTINGS--|--Home'
 export const GET_TOKENS_ALLOWED = 'GET_TOKENS_ALLOWED--|--Home'
-export const SET_METHOD_ARGS = 'SET_METHOD_ARGS--|--UDapp'
-export const CHANGE_SLIDER_VALUE = 'CHANGE_SLIDER_VALUE--|--UDapp'
-
+export const CHANGE_SLIDER_VALUE = 'CHANGE_SLIDER_VALUE--|--Home'
 export const SET_ETHEREUM_PROVIDER = 'SET_ETHEREUM_PROVIDER--|--Home'
 export const SET_CUSTOM_METHODS = 'SET_CUSTOM_METHODS--|--Home'
 export const REQUEST_MODAL_METHOD = 'REQUEST_MODAL_METHOD--|--Home'
@@ -19,36 +27,40 @@ export const CLICK_ACTION_REQUEST = 'CLICK_ACTION_REQUEST--|--Home'
 export const SET_WALLET = 'SET_WALLET--|--Home'
 export const SET_CONTRACTS = 'SET_CONTRACTS--|--Home'
 export const SET_MIN_DEPOSIT = 'SET_MIN_DEPOSIT--|--Home'
-export const SET_TOKENS_ALLOWED = 'SET_TOKENS_ALLOWED--|--Home'
-export const GET_ETH_PROVIDER = 'GET_ETH_PROVIDER--|--Home'
-export const SET_LOGS = 'SET_LOGS--|--Home'
-export const POLL_LOGS_REQUEST = 'POLL_LOGS_REQUEST--|--Home'
-export const SET_DECODED_LOGS = 'SET_DECODED_LOGS--|--Home'
-export const NEW_ARRAY = 'NEW_ARRAY--|--Home'
 
-export const TX_APPROVE = 'TX_APPROVE--|--Home'
-export const TX_APPLY = 'TX_APPLY--|--Home'
-export const TX_CHALLENGE = 'TX_CHALLENGE--|--Home'
-export const TX_COMMIT_VOTE = 'TX_COMMIT_VOTE--|--Home'
-export const TX_UPDATE_STATUS = 'TX_UPDATE_STATUS--|--Home'
-export const TX_CHECK_TEST= 'TX_CHECK_TEST--|--Home'
+// Contracts-general
+export const TX_CHECK_TEST = 'TX_CHECK_TEST--|--Contract'
+export const EXECUTE_METHOD_REQUEST = 'EXECUTE_METHOD_REQUEST--|--Contract'
+export const SET_ABI = 'SET_ABI--|--Contract'
+export const SET_METHOD_ARGS = 'SET_METHOD_ARGS--|--Contract'
+export const EXECUTE_METH = 'EXECUTE_METH--|--Contract'
+export const SET_LOGS = 'SET_LOGS--|--Contract'
+export const POLL_LOGS_REQUEST = 'POLL_LOGS_REQUEST--|--Contract'
+export const SET_DECODED_LOGS = 'SET_DECODED_LOGS--|--Contract'
 
-export const EV_APPLICATION = 'EV_APPLICATION--|--Home'
-export const EV_CHALLENGE = 'EV_CHALLENGE--|--Home'
-export const EV_TRANSFER = 'EV_TRANSFER--|--Home'
-export const EVENT_FROM_REGISTRY = 'EVENT_FROM_REGISTRY--|--Home'
-export const EVENT_FACEOFF= 'EVENT_FACEOFF--|--Home'
-export const EVENT_WHITELIST = 'EVENT_WHITELIST--|--Home'
-export const EXECUTE_METHOD_REQUEST = 'EXECUTE_METHOD_REQUEST--|--Home'
+// Token
+export const SET_TOKENS_ALLOWED = 'SET_TOKENS_ALLOWED--|--Token'
+export const EV_TRANSFER = 'EV_TRANSFER--|--Token'
+export const TX_APPROVE = 'TX_APPROVE--|--Token'
 
-export const NEW_ITEM = 'NEW_ITEM--|--Home'
-export const CHANGE_ITEM = 'CHANGE_ITEM--|--Home'
-export const CHANGE_ITEMS = 'CHANGE_ITEMS--|--Home'
+// Registry
+export const TX_APPLY = 'TX_APPLY--|--Registry'
+export const TX_CHALLENGE = 'TX_CHALLENGE--|--Registry'
+export const TX_UPDATE_STATUS = 'TX_UPDATE_STATUS--|--Registry'
+export const EV_APPLICATION = 'EV_APPLICATION--|--Registry'
+export const EV_CHALLENGE = 'EV_CHALLENGE--|--Registry'
+export const EVENT_FROM_REGISTRY = 'EVENT_FROM_REGISTRY--|--Registry'
+export const EVENT_FACEOFF = 'EVENT_FACEOFF--|--RegistryChallenge'
+export const EVENT_WHITELIST = 'EVENT_WHITELIST--|--RegistryWhitelist'
+
+// Voting
+export const TX_COMMIT_VOTE = 'TX_COMMIT_VOTE--|--Vote'
+export const TX_REVEAL_VOTE = 'TX_REVEAL_VOTE--|--Vote'
+
+// Store
+export const NEW_ARRAY = 'NEW_ARRAY--|--Reducer'
+export const NEW_ITEM = 'NEW_ITEM--|--Reducer'
+export const CHANGE_ITEM = 'CHANGE_ITEM--|--Reducer'
+export const CHANGE_ITEMS = 'CHANGE_ITEMS--|--Reducer'
 
 // UDapp
-export const UDAPP_ERROR = 'UDAPP_ERROR--|--UDapp'
-export const SET_ADDRESS = 'SET_ADDRESS--|--UDapp'
-export const SET_FROM_ADDRESS = 'SET_FROM_ADDRESS--|--UDapp'
-export const SET_ABI = 'SET_ABI--|--UDapp'
-export const REFRESH_ETH_STORE = 'REFRESH_ETH_STORE--|--UDapp'
-export const EXECUTE_METH = 'EXECUTE_METH--|--UDapp'
