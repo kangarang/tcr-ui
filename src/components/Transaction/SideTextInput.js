5fcbe299344a64c7136c81c74c4d3367290bcb3a
Isaac Kang <isaac.kang@consensys.net>
2018-03-06 21:02:56 -0500

    move side panel transaction components

---
+++ src/components/Transaction/SideTextInput.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import { Text, TextInput } from '@aragon/ui'
+import { Icon } from 'semantic-ui-react'
+
+const MarginDiv = styled.div`
+  margin: 1em 0;
+`
+export default ({ icon, title, text, small }) => (
+  <div>
+    <MarginDiv>
+      {icon && <Icon name={icon} size="small" />}
+      <Text color="grey" smallcaps={small}>
+        {title}
+      </Text>
+    </MarginDiv>
+    <MarginDiv>
+      <TextInput />
+    </MarginDiv>
+  </div>
+)
---
+++ src/components/Transaction/index.js
@@ -0,0 +1,5 @@
+import SideSplit from './SideSplit'
+import SideText from './SideText'
+import SideTextInput from './SideTextInput'
+
+export { SideSplit, SideText, SideTextInput }
