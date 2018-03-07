b77db4a3f7550c49b125f1287ae39b9d2970ca6c
Isaac Kang <isaac.kang@consensys.net>
2018-03-06 21:02:00 -0500

    mv fns to /components

---
+++ src/components/StyledHome.js
@@ -0,0 +1,44 @@
+import styled from 'styled-components'
+import { colors } from '../colors'
+
+export const AppBarWrapper = styled.div`
+  flex-shrink: 0;
+`
+export const AppBar = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 100%;
+  height: 4em;
+  background-color: ${colors.offWhite};
+  border-bottom: 1px solid ${colors.orange};
+  padding: 0 3em;
+  & > div {
+    margin: 0 1em;
+  }
+`
+export const MarginDiv = styled.div`
+  margin: 1em 0;
+`
+export const OverFlowDiv = styled.div`
+  overflow: hidden;
+  text-overflow: ellipsis;
+`
+export const HomeWrapper = styled.div`
+  padding: 2em;
+`
+
+export const CMItem = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 5px 20px;
+  cursor: pointer;
+  white-space: nowrap;
+  & > div {
+    padding: 5px;
+  }
+`
+export const FileInput = styled.input`
+  padding: 1em;
+  border: 2px solid ${colors.prism};
+`
---
+++ src/components/TopGradient.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+import { colors } from '../colors'
+
+const TopGradient = styled.div`
+  position: relative;
+  background: linear-gradient(to right, ${colors.prism}, ${colors.darkBlue});
+  height: 14px;
+  width: 100%;
+  z-index: 9;
+`
+
+export default TopGradient
