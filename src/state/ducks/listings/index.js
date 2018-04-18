8da58073d96fac0b85b683156aa50cf6178685cc
Isaac Kang <isaac.kang@consensys.net>
2018-04-18 17:43:02 -0400

    add test. add _abi util.

---
+++ src/state/ducks/listings/index.js
@@ -0,0 +1,9 @@
+import reducer from './reducers'
+
+// import * as listingSagas from './sagas'
+import * as listingSelectors from './selectors'
+import * as listingTypes from './types'
+
+export { listingSelectors, listingTypes }
+
+export default reducer
---
+++ src/state/libs/test/ipfs.test.js
@@ -0,0 +1,10 @@
+import { ipfsGetData } from '../ipfs'
+
+describe('libs: ipfs', async () => {
+  describe('function: ipfsGetData', () => {
+    test('should return the correct ipfs object id', async () => {
+      const data = await ipfsGetData('QmeCTiFp7VUgPBnxQcWfNjrxFeh9eeScSx8VUKE2344beo')
+      expect(data.id).toBe('Prospect Park')
+    })
+  })
+})
