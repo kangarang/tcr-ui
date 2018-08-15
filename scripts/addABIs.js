39d874ce8c5ed1043a13608753b47551d9542937
Isaac Kang <isaac.kang@consensys.net>
2018-08-15 18:31:56 -0400

    rm extraneous functional logic to add abis

--- scripts/addABIs.js
+++ scripts/addABIs.js
@@ -1,17 +1,14 @@
 import abis from './abis'
 import { ipfsAddObject } from 'libs/ipfs'
 
-async function addABIs() {
-  const data = {
-    id: 'Prospekt Park',
-    registry: abis.registry,
-    token: abis.token,
-    voting: abis.voting,
-    parameterizer: abis.parameterizer,
-  }
-  const hash = await ipfsAddObject(data)
-  console.log('object added to ipfs. hash:', hash)
-  return hash
+const data = {
+  id: 'Prospekt Park',
+  registry: abis.registry,
+  token: abis.token,
+  voting: abis.voting,
+  parameterizer: abis.parameterizer,
 }
 
-addABIs()
+ipfsAddObject(data).then(hash => {
+  console.log('object added to ipfs. hash:', hash)
+})
